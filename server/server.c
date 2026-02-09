321f0eb87addc1729e1b1838d5bbde59bd8b9f5b
LeoLegion <monkeydluffyonepiece114@gmail.com>
2026-02-10 00:13:28 +0530

    Add basic multithreaded TCP server

---
+++ server/server.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <arpa/inet.h>
+
+#define  PORT 8080
+
+void *handle_client(void *arg);
+
+int main() {
+    int server_fd, client_fd;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t addr_len = sizeof(client_addr);
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+
+    bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    listen(server_fd, 10);
+
+    printf("Server running on port %d...\n", PORT);
+
+    while (1) {
+        client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &addr_len);
+        
+        pthread_t tid;
+        int *pclient = malloc(sizeof(int));
+        *pclient = client_fd;
+
+        pthread_create(&tid, NULL, handle_client, pclient);
+        pthread_detach(tid);
+    }
+
+    return 0;
+}
+
+void *handle_client(void *arg) {
+    int client_fd = *((int *)arg);
+    free(arg);
+
+    char buffer[256];
+
+    while (1) {
+        int n = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
+        if (n <= 0)
+            break;
+        buffer[n] = '\0';
+        printf("Client says: %s\n", buffer);
+
+        send(client_fd, buffer, n, 0);
+    }
+
+    close(client_fd);
+    return NULL;
+}
