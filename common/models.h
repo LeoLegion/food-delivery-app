90d542e03f6b47508ce986e0311398b9464b05a3
LeoLegion <monkeydluffyonepiece114@gmail.com>
2026-02-09 23:43:25 +0530

    Initial project structure

---
+++ common/models.h
@@ -0,0 +1,34 @@
+#ifndef MODELS_H
+#define MODELS_H
+
+typedef struct {
+    int user_id;
+    char name[50];
+    char phone[15];
+    char password[20];
+} User;
+
+typedef struct {
+    int restaurant_id;
+    char name[50];
+} Restaurant;
+
+typedef struct {
+    int item_id;
+    int restaurant_id;
+    char name[50];
+    float price;
+} MenuItem;
+
+typedef struct {
+    int order_id;
+    int user_id;
+    int restaurant_id;
+    int item_id;
+    int quantity;
+    float total_price;
+    int status; // 0=PLACED,1=PREPARING,2=DELIVERED
+} Order;
+
+#endif
+
---
+++ common/protocol.h
@@ -0,0 +1,21 @@
+#ifndef PROTOCOL_H
+#define PROTOCOL_H
+
+#define CMD_REGISTER  1
+#define CMD_LOGIN     2
+#define CMD_VIEW_MENU 3
+#define CMD_ORDER     4
+#define CMD_EXIT      5
+
+typedef struct {
+    int command;
+    char payload[256];
+} Request;
+
+typedef struct {
+    int status; // 0=FAIL, 1=SUCCESS
+    char message[256];
+} Response;
+
+#endif
+
